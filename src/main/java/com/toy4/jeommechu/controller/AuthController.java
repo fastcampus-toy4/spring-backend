05d18205fe3e524762757a6e10d9f275920b5075
dongjae <dj8@naver.com>
2025-07-31 11:43:10 +0900

    D

--- src/main/java/com/toy4/jeommechu/controller/AuthController.java
+++ src/main/java/com/toy4/jeommechu/controller/AuthController.java
@@ -16,7 +16,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
+@CrossOrigin(origins = "http://react-frontend:3000", allowCredentials = "true")
 public class AuthController {
 
     private final UserRepository userRepo;
