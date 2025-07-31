da71ab9687659e857b6278888ce78595f22c260a
Seungmin <rnsdls1997@gmail.com>
2025-07-31 16:37:17 +0900

    chore: web config cors 임시 비활성화

--- src/main/java/com/toy4/jeommechu/config/WebConfig.java
+++ src/main/java/com/toy4/jeommechu/config/WebConfig.java
@@ -4,18 +4,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("http://react-frontend:80",
-                        "http://localhost:80",
-                        "http://155.248.175.96:3000",
-                        "http://155.248.175.96:8000",
-                        "http://155.248.175.96:9000"
-                        )
-                .allowCredentials(true)
-                .allowedMethods("GET", "POST", "PUT", "DELETE");
-    }
-}
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/api/**")
+//                .allowedOrigins("http://react-frontend:80",
+//                        "http://localhost:80",
+//                        "http://155.248.175.96:3000",
+//                        "http://155.248.175.96:8000",
+//                        "http://155.248.175.96:9000"
+//                        )
+//                .allowCredentials(true)
+//                .allowedMethods("GET", "POST", "PUT", "DELETE");
+//    }
+//}
