ddc37f4de59a4b1a6c34cf2f92d0d85cf2c38dea
dongjae <dj8@naver.com>
2025-07-31 14:11:25 +0900

    1

--- src/main/java/com/toy4/jeommechu/config/WebConfig.java
+++ src/main/java/com/toy4/jeommechu/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000",
+                .allowedOrigins("http://react-frontend:3000",
                         "http://:155.248.175.96:3000")
                 .allowCredentials(true)
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
